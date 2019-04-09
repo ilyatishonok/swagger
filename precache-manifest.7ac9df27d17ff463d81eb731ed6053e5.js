eb3f5af60af60db2c9c5e0c0b9a9cebf27384e6b
Ilya <tishonok.ilya@ya.ru>
2019-04-09 03:17:43 +0300

    Updates

---
+++ precache-manifest.7ac9df27d17ff463d81eb731ed6053e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4828585049bbd6ffe25a",
+    "url": "/swagger/static/js/runtime~main.9ad01a72.js"
+  },
+  {
+    "revision": "4563576c97b5330437dd",
+    "url": "/swagger/static/js/main.dfe43b4a.chunk.js"
+  },
+  {
+    "revision": "005d85d9174c0bc4b9cf",
+    "url": "/swagger/static/js/2.8e5fc4b2.chunk.js"
+  },
+  {
+    "revision": "005d85d9174c0bc4b9cf",
+    "url": "/swagger/static/css/2.349a6628.chunk.css"
+  },
+  {
+    "revision": "389ed74a1d62a141a517f3414b11fd6f",
+    "url": "/swagger/index.html"
+  }
+];
